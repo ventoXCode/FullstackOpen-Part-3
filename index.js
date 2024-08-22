1d51c8f8e113015038432a0ea8328e881012fd76
Joris Geis <87895936+ventoXCode@users.noreply.github.com>
2024-08-22 13:27:40 +0200

    Add middleware

--- index.js
+++ index.js
@@ -12,21 +12,47 @@ const url = process.env.MONGODB_URI;
 mongoose.set("strictQuery", false);
 mongoose.connect(url);
 
+app.use(express.static("dist"));
 app.use(express.json());
 app.use(morgan("tiny"));
 app.use(cors());
-app.use(express.static("dist"));
 
-app.get("/api/persons/:id", (request, response) => {
-  Person.findById(request.params.id).then((person) => {
-    response.json(person);
-  });
+app.get("/api/notes/:id", (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
+
 app.delete("/api/persons/:id", (request, response) => {
-  Person.findById(request.params.id).then((person) => {
-    response.json(person).status(204).end();
-  });
+  Person.findByIdAndDelete(request.params.id)
+    .then((person) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
 app.post("/api/persons", (request, response) => {
